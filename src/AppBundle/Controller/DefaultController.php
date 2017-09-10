da11b887ce5eb0c248373c29a58622233f51c3d4
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2017-09-10 14:11:33 +0200

    feat: communication

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,46 +15,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $articles = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->getArticles(time(), 3)
-            ->getResult();
-
-        $jsonArticles = [];
-        foreach ($articles as $article) {
-            $jsonArticles[] = [
-                'id' => $article->getId(),
-                'title' => $article->getTitle(),
-                'createdAt' => $article->getCreatedAt()->getTimestamp()
-            ];
-        }
-
-        return $this->render('default/index.html.twig', [
-            'latestNews' => $jsonArticles,
-            'lastItemDate' => $jsonArticles[count($jsonArticles) - 1]['createdAt']
-        ]);
-    }
-
-    /**
-     * @Route("/ws", name="ws")
-     */
-    public function wsAction(Request $request)
-    {
-        $lastItemDate = (int) ($request->get('last_item_date') ?? time());
-        $articles = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->getArticles($lastItemDate, 3)
-            ->getResult();
-
-        $jsonArticles = [];
-        foreach ($articles as $article) {
-            $jsonArticles[] = [
-                'id' => $article->getId(),
-                'title' => $article->getTitle(),
-                'createdAt' => $article->getCreatedAt()->getTimestamp()
-            ];
-        }
-
-        return new JsonResponse($jsonArticles);
+        return $this->render('default/index.html.twig', []);
     }
 }
